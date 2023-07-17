cbae99fe11d87ef79662a7dd678413cfdb108759
Redyf <mateusalvespereira7@gmail.com>
2023-07-17 12:31:41 -0300

    Fixed sha256

--- flake.nix
+++ flake.nix
@@ -10,28 +10,31 @@
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      defaultPackage = pkgs.symlinkJoin {
-        name = "myfonts-0.1.4";
-        paths =
-          builtins.attrValues
-          self.packages.${system}; # Add font derivation names here
-      };
-
-      packages.times = pkgs.stdenvNoCC.mkDerivation {
-        name = "times-font";
-        dontConfigue = true;
-        src = pkgs.fetchzip {
-          url = "https://www.freebestfonts.com/download?fn=1911";
-          sha256 = "0000000000000000000000000000000000000000000000000000";
-          stripRoot = false;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        defaultPackage = pkgs.symlinkJoin {
+          name = "myfonts-0.1.4";
+          paths =
+            builtins.attrValues
+            self.packages.${system}; # Add font derivation names here
         };
-        installPhase = ''
-          mkdir -p $out/share/fonts
-          cp -R $src $out/share/fonts/opentype/
-        '';
-        meta = {description = "A Gill Sans Font Family derivation.";};
-      };
-    });
+
+        packages.times =
+          pkgs.stdenvNoCC.mkDerivation {
+            name = "times-font";
+            dontConfigue = true;
+            src =
+              pkgs.fetchzip {
+                url = "https://www.freebestfonts.com/download?fn=1911";
+                sha256 = "0mbwf83n4kikd4dy9mph1xcwmyy6q9x4
+mpa5cy6kziix736c4l0r";
+                stripRoot = false;
+              };
+            installPhase = ''
+              mkdir -p $out/share/fonts
+              cp -R $src $out/share/fonts/opentype/
+            '';
+            meta = {description = "A Gill Sans Font Family derivation.";};
+          };
+      });
 }
